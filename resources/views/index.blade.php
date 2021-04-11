b034231291ed505e95ddfe5aeb0717e86c452b08
phillipkregg <phillipkregg@gmail.com>
2021-04-10 22:54:57 -0500

    created add-an-idea form and added more styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -100,7 +100,7 @@
             </div>
             <div class="flex px-2 py-6">
                 <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="" class="w-14 h-14 rounded-xl">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="" class="w-14 h-14 rounded-xl">
                 </a>
                 <div class="mx-4">
                     <h4 class="text-xl font-semibold">
@@ -118,8 +118,65 @@
                             <div class="text-gray-900">22 Comments</div>
                         </div>
                         <div class="flex items-center space-x-2">
-                            <div class="px-4 py-2 font-bold leading-none text-center text-white uppercase rounded-full bg-green text-xxs w-28 h-7">
+                            <div class="px-4 py-2 font-bold leading-none text-center text-white uppercase rounded-full bg-yellow text-xxs w-28 h-7">
                                 In Progress
+                            </div>
+                            <button class="relative px-3 py-1 leading-loose text-gray-400 transition duration-150 ease-in bg-gray-100 rounded-full pb-7 hover:bg-gray-200 h-7">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                                <ul class="absolute py-3 ml-8 font-semibold text-left bg-white shadow-dialog w-44 rounded-xl">
+                                    <li>
+                                        <a href="#" class="block px-5 py-3 transition duration-150 hover:bg-gray-100">Mark as Spam</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="block px-5 py-3 transition duration-150 hover:bg-gray-100">Delete</a>
+                                    </li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="my-6 space-y-6 ideas-container">
+        <div class="flex transition duration-150 ease-in bg-white cursor-pointer idea-container hover:shadow-card rounded-xl">
+            <div class="px-5 py-8 border-r border-gray-100">
+                <div class="text-center">
+                    <div class="text-2xl font-semibold">
+                        3
+                    </div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 px-4 py-3 font-bold uppercase transition duration-150 ease-in bg-gray-200 border border-gray-200 text-xxs hover:border-gray-400 rounded-xl">vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=3" alt="" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">More randomness in titles goes here</a>
+                    </h4>
+                    <div class="mt-3 text-gray-600 line-clamp-3">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio alias corrupti magni illo, expedita delectus ipsam reiciendis enim eius consequuntur ratione neque saepe commodi nesciunt laborum doloribus explicabo voluptas pariatur.
+                    </div>
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center space-x-2 text-xs font-semibold text-gray-400">
+                            <div>2 days ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">22 Comments</div>
+                        </div>
+                        <div class="flex items-center space-x-2">
+                            <div class="px-4 py-2 font-bold leading-none text-center text-white uppercase rounded-full bg-red text-xxs w-28 h-7">
+                                Closed
                             </div>
                             <button class="relative px-3 py-1 leading-loose text-gray-400 transition duration-150 ease-in bg-gray-100 rounded-full pb-7 hover:bg-gray-200 h-7">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,48 @@
 
     <main class="container flex mx-auto max-w-custom" >
         <div class="mr-5 w-70">
-            Add Idea form goes here
+            <div
+                class="mt-16 bg-white border-2 border-blue rounded-xl"
+                style="border-image-source: linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                    border-image-slice: 1;
+                    background-image: linear-gradient(to bottom, #ffffff, #ffffff), linear-gradient(to bottom, rgba(50, 138, 241, 0.22), rgba(99, 123, 255, 0));
+                    background-origin: border-box;
+                    background-clip: content-box, border-box;"
+
+            >
+                <div class="px-6 py-2 pt-6 text-center">
+                    <h3 class="text-base font-simibold">Add an Idea</h3>
+                    <p class="mt-4 text-x">Let us know what you would like, and we'll take a look at it!</p>
+                </div>
+
+                <form action="" method="POST" class="px-4 py-6 space-y-4 ">
+                    <div>
+                        <input type="text" class="w-full px-4 py-2 text-sm placeholder-gray-400 bg-gray-100 border-none rounded-xl" placeholder="Your idea">
+                    </div>
+                    <div>
+                        <select name="category" id="category_add" class="w-full px-4 py-2 bg-gray-100 border-none rounded-xl">
+                            <option value="Category One">Category One</option>
+                            <option value="Category Two">Category Two</option>
+                            <option value="Category Three">Category Three</option>
+                            <option value="Category Four">Category Four</option>
+                        </select>
+                    </div>
+                    <div>
+                        <textarea name="idea" id="idea" cols="30" rows="4" placeholder="Describe your idea" class="w-full px-4 py-2 text-sm placeholder-gray-400 bg-gray-100 border-none rounded-xl"></textarea>
+                    </div>
+                    <div class="flex items-center justify-between space-x-3">
+                        <button type="button" class="flex items-center justify-center w-1/2 px-6 py-3 text-xs font-semibold transition duration-150 ease-in bg-gray-200 border border-gray-200 h-11 rounded-xl hover:border-gray-400">
+                            <svg class="w-4 text-gray-600 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                            </svg>
+                            <span class="ml-1">Attach</span>
+                        </button>
+                        <button type="submit" class="flex items-center justify-center w-1/2 px-6 py-3 text-xs font-semibold text-white transition duration-150 ease-in border border-gray-200 bg-blue h-11 rounded-xl hover:bg-blue-hover">
+                            <span class="ml-1">Submit</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
 
         <div class="w-175">
@@ -82,6 +123,7 @@
 
 
         </div>
+
     </main>
 </body>
 
